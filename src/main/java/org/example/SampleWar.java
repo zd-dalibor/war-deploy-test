2855a2fc725af316345f040dc4db1f0308e93ef7
Dalibor Zdravkovic <dzdravkovic@katabat.com>
2021-03-19 00:12:11 +0100

    Sample repo for publishing artifacts

---
+++ src/main/java/org/example/SampleWar.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class SampleWar {
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
